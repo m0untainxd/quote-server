9215efc74af99b801e5df8cc0feec3127dd84c7e
mountain <thesailorgamer0@gmail.com>
2023-04-12 18:04:00 +0100

    add quote mongoose schema

---
+++ backend/models/quote.model.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const QuoteSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: 'Name of quote is required'
+    },
+    total: {
+        type: mongoose.Types.Decimal128,
+        required: 'Total price is required'
+    },
+    people: [{
+            name: {
+                type: String,
+                required: "Name of person required"
+            },
+            cost: {
+                type: mongoose.Types.Decimal128,
+                required: "Cost of person required"
+            }
+    }],
+    resources: [{
+        name: {
+            type: String,
+            required: "Name of physical resource required"
+        },
+        res_type: {
+            type: String,
+            required: "Type of cost for physical resource required"
+        },
+        number: Number,
+        cost: {
+            type: mongoose.Types.Decimal128,
+            required: "Cost of physical resource required"
+        }
+    }],
+    updated: Date,
+    created: {
+        type: Date,
+        default: Date.now()
+    }
+})
